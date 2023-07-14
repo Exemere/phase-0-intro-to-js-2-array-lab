b2e9a87306cd3e519d621438aed050f89a644104
Livosero Esemere Nicolas <nesemere55@gmail.com>
2023-07-14 15:14:34 +0300

    completed assighnment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+function appendCat(name) {
+  return cats.push(name);
+}
+
+function prependCat(name) {
+  return [name].shift(cats);
+}
+
+function removeLastCat() {
+  return cats.slice(0, -1);
+}
+
+function removeFirstCat() {
+  return cats.slice(1);
+}
+
+beforeEach(() => {
+  cats.length = 0;
+  cats.push("Milo", "Otis", "Garfield");
+});
